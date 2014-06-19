c17d706edfd99593eb362b5d7c4c9a9af9c0a87d
ET-NiK <kronstadtsky@bk.ru>
2014-06-19 18:43:03 +0400

    Загрузка файлов модуля.

---
+++ application/modules/notes/controllers/notes.php
@@ -0,0 +1,93 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Notes for GameAP
+ *
+ * 
+ *
+ * @package		Notes
+ * @author		Nikita Kuznetsov (ET-NiK)
+ * @copyright	Copyright (c) 2013-2014, Nikita Kuznetsov (http://hldm.org)
+ * @license		http://www.gameap.ru/license.html
+ * @link		http://www.gameap.ru
+ * @filesource
+*/
+
+class Notes extends MX_Controller {
+	
+	var $tpl_data 	= array();
+	
+	/* Автоматически загружаемые модели */
+	var $autoload = array('model' => array('users'), );
+	
+	// -----------------------------------------------------------------
+	
+	public function __construct()
+    {
+        parent::__construct();
+        
+        if (!$this->users->check_user()) {
+			return;
+		}
+
+        /* Load DataBase */
+		$this->load->database();
+		
+		/* Загрузка языковых файлов */
+		$this->lang->load('server_files');
+        $this->lang->load('server_command');
+		
+		/* Templates */
+		$this->tpl_data['title'] = 'GameAP :: Notes';
+		$this->tpl_data['heading'] = 'Notes';
+		$this->tpl_data['menu'] = $this->parser->parse('menu.html', $this->tpl_data, TRUE);
+		$this->tpl_data['profile'] = $this->parser->parse('profile.html', $this->users->tpl_userdata(), true);
+    }
+    
+    // -----------------------------------------------------------------
+    
+    public function load()
+    {
+		/* Авторизован ли юзер */
+		if (!$this->users->auth_data) {
+			exit;
+		}
+		
+		if (isset($this->users->auth_data['modules_data']['notes'])) {
+			$notes = $this->users->auth_data['modules_data']['notes'];
+		} else {
+			$notes = '';
+		}
+		
+		$this->output->set_output($notes);
+	}
+    
+    // -----------------------------------------------------------------
+    
+    public function save()
+    {
+		/* Авторизован ли юзер */
+		if (!$this->users->auth_data) {
+			$this->output->append();
+			exit;
+		}
+		
+		$note = htmlspecialchars($this->input->post('note', true));
+		$this->users->update_modules_data($this->users->user_id, $note, 'notes');
+		$this->output->set_output(1);
+	}
+    
+    // -----------------------------------------------------------------
+    
+    public function index()
+    {
+		/* Авторизован ли юзер */
+        if (!$this->users->auth_data) {
+			redirect('auth/in');
+		}
+		
+		$local_tpl = array();
+		$this->tpl_data['content'] = $this->parser->parse('notes.html', $local_tpl, TRUE);
+        $this->parser->parse('main.html', $this->tpl_data);
+	}
+
+}
---
+++ application/modules/notes/module_info.php
@@ -0,0 +1,13 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$module_info['name'] 			= 'Заметки';											// Имя модуля
+$module_info['description'] 	= 'Записная книжка для пользователей';					// Описание
+$module_info['version'] 		= '1.0';												// Версия модуля
+$module_info['update_info'] 	= 'http://www.gameap.ru/updates/notes.txt';				// Информация о новых версиях
+$module_info['show_in_menu'] 	= 1;													// Не показывать в меню
+$module_info['access'] 			= 'user';
+$module_info['developer'] 		= 'ET-NiK';
+$module_info['site'] 			= 'http://hldm.org';
+$module_info['email'] 			= 'nikita@hldm.org';
+$module_info['copyright'] 		= '(c) 2013-2014, ET-NiK (http://hldm.org)';
+$module_info['license'] 		= 'http://www.gameap.ru/license.html';
